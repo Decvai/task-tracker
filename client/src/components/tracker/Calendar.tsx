eb81939c2e64492143824cf81f129ab7ad17f67d
Cvai <vladyslav.katrych.main@gmail.com>
2021-05-11 18:51:58 +0300

    basic calendar integrated

--- client/src/components/tracker/Calendar.tsx
+++ client/src/components/tracker/Calendar.tsx
@@ -1,5 +1,35 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
+
 const Calendar = () => {
-	return <div className='calendar'>Calendar</div>;
+	function handleDateClick(arg: DateClickArg) {
+		alert(arg.dateStr);
+	}
+
+	return (
+		<div className='calendar'>
+			<div className='calendar__header'>
+				<div className='calendar__new-task'>
+					<button>Create new task</button>
+				</div>
+			</div>
+			<div className='calendar__body'>
+				<div className='calendar__current-date'>
+					<div className='calendar__time'>12:32:20 AM</div>
+					<div className='calendar__current-day'>
+						Sunday, May 9, 2021
+					</div>
+				</div>
+
+				<FullCalendar
+					plugins={[dayGridPlugin, interactionPlugin]}
+					initialView='dayGridMonth'
+					dateClick={handleDateClick}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Calendar;
--- client/src/components/tracker/Sidebar.tsx
+++ client/src/components/tracker/Sidebar.tsx
@@ -1,5 +1,12 @@
 const Sidebar = () => {
-	return <div className='sidebar'>Sidebar</div>;
+	return (
+		<div className='sidebar'>
+			<div className='sidebar__profile'>
+				<div className='sidebar__profile-icon'>Profile icon</div>
+			</div>
+			<div className='sidebar__achievements'>Achievements</div>
+		</div>
+	);
 };
 
 export default Sidebar;
